1fb320b01bf93d161e74efff9ee45053700d7518
rkline2 <rkline2@linux3.gl.umbc.edu>
2020-10-28 18:03:11 -0400

    version 0.2

--- replace.asm
+++ replace.asm
@@ -4,29 +4,36 @@ request:        db "Enter a location in the string you want to replace 1-16",10
 len_r:          equ $-request
 original_mssg:  db "Here is the original string",10
 random_mssg:    db "Here is the string with the random character updated",10
-
+	
 new_line:       db      10
         section	.bss
+random_val:	resb 	2
 num_buff:       resb    2
 char_buff:      resb    1
-
+seed:	 	resb 	2
+	
         section .text
         global main
 
 main:
-        mov rax,1
+        mov rax,1		; displaying question
         mov rdi,1
         mov rsi,request
         mov rdx,len_r
         syscall
 
-        mov rax,0
-        mov rdi,0
+        mov rax,0		; storing in num to rsi
+        mov rdi,0		
         mov rsi,num_buff
-        mov rdx,2
+        mov rdx,3
         syscall
 
+	mov rax,1 		; displaying stored num 
+	mov rdi,1
+	mov rsi,num_buff
+	syscall
 
+	;; instead of mod use divq: quotient stored in rax, remainder stored in rdx
 exit:
         mov rax,60
         xor rdi,rdi
