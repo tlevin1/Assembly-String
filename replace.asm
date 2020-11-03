2c151162fe23fb9e738736a75b4d939a446e0dcb
tlevin1 <tlevin1@linux1.gl.umbc.edu>
2020-11-02 20:34:39 -0500

    Version 1.1

--- replace.asm
+++ replace.asm
@@ -1,66 +1,328 @@
-        ;; Project 3
-        section .data
+	; Project 3
+	;Purpose: write and assemble a program to prompt the user for a number from 1-16 and display the unedited string to the user and then  display the string with location entered replaced with a random letter
+         section .data		
 request:        db "Enter a location in the string you want to replace 1-16",10
 len_r:          equ $-request
 original_mssg:  db "Here is the original string",10
+len_o:		equ $-original_mssg
 random_mssg:    db "Here is the string with the random character updated",10
-	
+len_m:		equ $-random_mssg
 new_line:       db      10
+random_string	db "................"
+len_s:		equ $-random_string
         section	.bss
-random_val:	resb 	2
 num_buff:       resb    2
-char_buff:      resb    1
-seed:	 	resb 	2
-test:		resb 	2
+char_buff:  	resb 	2
+seed:		resb 	2
         section .text
         global main
 
 main:
+	mov r8,random_string
         mov rax,1		; displaying question
         mov rdi,1
         mov rsi,request
         mov rdx,len_r
         syscall
 
-        mov rax,0		; storing in num to rsi
+        mov rax,0		; storing entered num in rsi
         mov rdi,0		
         mov rsi,num_buff
         mov rdx,3
         syscall
 
-	mov rax,1 		; displaying stored num 
-	mov rdi,1
-	mov rsi,num_buff
-	mov rdx,2
-	syscall
 
 	mov rax,1
 	mov rdi,1
-	mov rsi,new_line
+	mov rsi,new_line	;displaying new line
 	mov rdx,1
 	syscall
 	
+	mov rax,1               ; displaying original message
+        mov rdi,1
+        mov rsi,original_mssg
+        mov rdx,len_o
+        syscall
 
-	xor rdx,rdx
-	RDRAND rax		; divide part is here
-	mov rbx,7
+        mov rax,1
+        mov rdi,1
+        mov rsi, new_line
+        mov rdx,1
+
+	
+	xor rdx,rdx		;1st LETTER
+	RDRAND rax		; divide part is here for first letter
+	mov rbx,25		;changed to 25 for alphabet 0-25/ a-z
+
 	div rbx
 	mov rbx,rdx
 
-	add rbx,48
-	mov [test],rbx
+	add rbx,97		;97 for ascii letter lowercase a -  range 97-122
+	mov [seed],rbx
 
-	mov rax,1
-	mov rdi,1
-	mov rsi,test
-	mov rdx,2
-	syscall
-		
-	mov rax,1
-	mov rdi,1
-	mov rsi,new_line
-	mov rdx,1
-	syscall
+	
+			
+	mov r9, [seed]	
+	mov [r8],r9		
+	inc r8			
+	
+	
+	xor rdx,rdx	       ;2nd LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+	
+	
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;3rd LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;4th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+	
+	xor rdx,rdx            ;5th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+	
+	xor rdx,rdx            ;6th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;7th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+	
+	xor rdx,rdx            ;8th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;9th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	
+        mov rax,1
+        mov rdi,1
+        mov rsi,random_string            ;print entire string
+        mov rdx,len_s
+        syscall
+
+	xor rdx,rdx            ;10th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;11th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;12th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;13th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;14th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;15th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+	xor rdx,rdx            ;16th LETTER
+        RDRAND rax              ; divide part is here for second letter
+        mov rbx,25              ;changed to 25 for alphabet 0-25/ a-z
+
+        div rbx
+        mov rbx,rdx
+
+        add rbx,97              ;97 for ascii letter lowercase a -  range 97-122
+        mov [seed],rbx
+
+
+
+        mov r9, [seed]
+        mov [r8],r9
+        inc r8
+
+
+        mov rax,1
+        mov rdi,1
+        mov rsi,new_line	;displaying new line at end
+        mov rdx,1
+        syscall
+
+
+
 
 	;; instead of mod use divq: quotient stored in rax, remainder stored in rdx
 exit:
